bc97db5941c69b4f74e6a85f065222617938d43c
Thomas Green <tomgreen135@gmail.com>
2019-06-24 12:58:50 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -18,7 +18,7 @@ function Header({ siteTitle }) {
           >
             <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
           </svg>
-          <span className="font-bold text-xl tracking-tight">{siteTitle}</span>
+          <span className="font-bold text-xl tracking-tight">{siteTitle} <span className="text-sm sm:hidden">Reg Charity Commission No. 1062657<span></span>
         </Link>
 
         <button
